fe69f5d387a59dca16d5a619ce3139d875e42981
tomhoskins <tomhoskins0712@gmail.com>
2025-12-03 20:55:32 -0600

    Shooting asteroids now removes them and the shot that hit it

--- main.py
+++ main.py
@@ -39,6 +39,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
